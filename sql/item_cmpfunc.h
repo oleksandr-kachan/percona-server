e809cc784d9aaa8573bdee623c409fa7a4f943be
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-10 10:14:57 +0200

    [sql] PS-269: Initial Percona Server 8.0.12 tree
    PS-5820: Crashes with the slow query log enabled
    
    Issue: there is a race condition between different threads calling
    Bloom_filter::clear and Bloom_filter::test_and_set.
    Before the fix for PS-5581, races always resulted in test_and_set
    returning false. After the fix, clear is no longer a single pointer
    change, and test_and_set could also return true in these situations.
    
    Root cause: PS-5581, 715f78f
    
    Workaround: restore the previous behavior by switching the single
    MEM_ROOT allocated Bitset a per THD locked raw pointer. The race is
    still there, but the results should be the same as before PS-5581.
    
    (cherry-picked from commit bde9bf2468188fe5d31071fa32c158bc100a3b2c)
    
    PS-5813: Fixed handling of 'none' value for slow_query_log_use_global_control server variable
    
    https://jira.percona.com/browse/PS-5813
    
    slow_query_log_use_global_control variable was implemented as Sys_var_set class. The purpose of 'none' value is to clear the set, but for Sys_var_set it is done by providing empty value. Specialized class implemented that detects 'none' literal.
    
    [sql] PS-5674: gen_lex_token generator reworked
    
    Percona tokens move to the end of the list, because they do not fit into spare area provided by upstream anymore.

--- sql/item_cmpfunc.h
+++ sql/item_cmpfunc.h
@@ -1397,6 +1397,8 @@ struct interval_range {
   my_decimal dec;
 };
 
+#ifdef MYSQL_SERVER
+
 class Item_func_interval final : public Item_int_func {
   typedef Item_int_func super;
 
@@ -2442,6 +2444,8 @@ class Item_func_like final : public Item_bool_func2 {
   bool check_covering_prefix_keys(THD *thd);
 };
 
+#endif /* MYSQL_SERVER */
+
 class Item_cond : public Item_bool_func {
   typedef Item_bool_func super;
 
@@ -2486,6 +2490,7 @@ class Item_cond : public Item_bool_func {
 
   Type type() const override { return COND_ITEM; }
   List<Item> *argument_list() { return &list; }
+  const List<Item> *argument_list() const { return &list; }
   bool eq(const Item *item, bool binary_cmp) const override;
   table_map used_tables() const override { return used_tables_cache; }
   void update_used_tables() override;
@@ -2507,6 +2512,8 @@ class Item_cond : public Item_bool_func {
   bool ignore_unknown() const { return abort_on_null; }
   bool equality_substitution_analyzer(uchar **) override { return true; }
 };
+
+#ifdef MYSQL_SERVER
 
 /*
   The class Item_equal is used to represent conjunctions of equality
@@ -2861,5 +2868,7 @@ bool WalkConjunction(Item *condition, Func func) {
     return func(condition);
   }
 }
+
+#endif /* MYSQL_SERVER */
 
 #endif /* ITEM_CMPFUNC_INCLUDED */
