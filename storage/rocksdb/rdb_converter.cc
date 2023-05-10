facebook/mysql-5.6

Support TIMESTAMP datatype for explicit MyRocks TTL columns
Summary:
Allow supporting MyRocks explicit ttl columns using TIMESTAMP data
fields.

MySQL's TIMESTAMP packed field consists of 4 to 7 bytes, where the first
4 bytes are the unix_timestamp and the next 3 optional bytes are the
fractional seconds. MyRocks TTL support can convert the first 4 bytes to
an internal TTL field and use it for expiring the row.

Reviewed By: lth

Differential Revision: D31703554

fbshipit-source-id: 627a476dff2

storage/rocksdb/rdb_converter.cc
@@ -728,10 +728,30 @@ int Rdb_converter::encode_value_slice(
     if (has_ttl_column) {
       DBUG_ASSERT(pk_def->get_ttl_field_index() != UINT_MAX);
       Field *const field = m_table->field[pk_def->get_ttl_field_index()];
-      DBUG_ASSERT(field->pack_length_in_rec() == ROCKSDB_SIZEOF_TTL_RECORD);
-      DBUG_ASSERT(field->real_type() == MYSQL_TYPE_LONGLONG);
+      DBUG_ASSERT(field->real_type() == MYSQL_TYPE_LONGLONG ||
+                  field->type() == MYSQL_TYPE_TIMESTAMP);
 
-      uint64 ts = uint8korr(field->ptr);
+      uint64 ts;
+
+      if (field->type() == MYSQL_TYPE_TIMESTAMP) {
+        /*
+          Timestamp packed length can be 4 to 7 bytes depending on precision.
+          The first four bytes is the unix time network byte order and is used
+          for MYSQL_TYPE_TIMESTAMP and MYSQL_TYPE_TIMESTAMP2. The optional
+          remaining bytes are for fractional seconds. Only the first four bytes
+          are needed.
+         */
+        DBUG_ASSERT(field->real_type() == MYSQL_TYPE_TIMESTAMP ||
+                    field->real_type() == MYSQL_TYPE_TIMESTAMP2);
+        DBUG_ASSERT(field->pack_length_in_rec() >= 4);
+        DBUG_ASSERT(field->key_type() == HA_KEYTYPE_BINARY);
+        ts = (uint64)rdb_netbuf_to_uint32(field->ptr);
+      } else {
+        DBUG_ASSERT(field->key_type() == HA_KEYTYPE_ULONGLONG);
+        DBUG_ASSERT(field->pack_length_in_rec() == ROCKSDB_SIZEOF_TTL_RECORD);
+        ts = uint8korr(field->ptr);
+      }
+
 #ifndef DBUG_OFF
       ts += rdb_dbug_set_ttl_rec_ts();
 #endif

storage/rocksdb/rdb_datadic.cc
@@ -500,8 +500,9 @@ void Rdb_key_def::setup(const TABLE *const tbl,
         */
         if (!m_ttl_column.empty() &&
             field->check_field_name_match(m_ttl_column.c_str())) {
-          DBUG_ASSERT(field->real_type() == MYSQL_TYPE_LONGLONG);
-          DBUG_ASSERT(field->key_type() == HA_KEYTYPE_ULONGLONG);
+          DBUG_ASSERT((field->real_type() == MYSQL_TYPE_LONGLONG &&
+                       field->key_type() == HA_KEYTYPE_ULONGLONG) ||
+                      field->type() == MYSQL_TYPE_TIMESTAMP);
           DBUG_ASSERT(!field->real_maybe_null());
           m_ttl_pk_key_part_offset = dst_i;
         }
@@ -628,8 +629,9 @@ uint Rdb_key_def::extract_ttl_col(const TABLE *const table_arg,
     for (uint i = 0; i < table_arg->s->fields; i++) {
       Field *const field = table_arg->field[i];
       if (field->check_field_name_match(ttl_col_str.c_str()) &&
-          field->real_type() == MYSQL_TYPE_LONGLONG &&
-          field->key_type() == HA_KEYTYPE_ULONGLONG &&
+          (field->type() == MYSQL_TYPE_TIMESTAMP ||
+           (field->real_type() == MYSQL_TYPE_LONGLONG &&
+            field->key_type() == HA_KEYTYPE_ULONGLONG)) &&
           !field->real_maybe_null()) {
         *ttl_column = ttl_col_str;
         *ttl_field_index = i;

oleksandr-kachan/percona-server

Upstream commit ID : fb-mysql-5.6.35/dd1c8130cbf7e582d1fa3525b1cc8be47abafdba
PS-8216 : Merge percona-202109
Ported from 5.6.
Also fixed warnings by updating some MyRocks tests to use 8.0 preferred
syntax and collations.
Differential Revision: D32223240
fbshipit-source-id: 95de8033751

storage/rocksdb/rdb_converter.cc
@@ -755,10 +755,30 @@ int Rdb_converter::encode_value_slice(
     if (has_ttl_column) {
       assert(pk_def->get_ttl_field_index() != UINT_MAX);
       Field *const field = m_table->field[pk_def->get_ttl_field_index()];
-      assert(field->pack_length_in_rec() == ROCKSDB_SIZEOF_TTL_RECORD);
-      assert(field->real_type() == MYSQL_TYPE_LONGLONG);
+      assert(field->real_type() == MYSQL_TYPE_LONGLONG ||
+             field->type() == MYSQL_TYPE_TIMESTAMP);
 
-      uint64 ts = uint8korr(field->field_ptr());
+      uint64 ts;
+
+      if (field->type() == MYSQL_TYPE_TIMESTAMP) {
+        /*
+          Timestamp packed length can be 4 to 7 bytes depending on precision.
+          The first four bytes is the unix time network byte order and is used
+          for MYSQL_TYPE_TIMESTAMP and MYSQL_TYPE_TIMESTAMP2. The optional
+          remaining bytes are for fractional seconds. Only the first four bytes
+          are needed.
+         */
+        assert(field->real_type() == MYSQL_TYPE_TIMESTAMP ||
+               field->real_type() == MYSQL_TYPE_TIMESTAMP2);
+        assert(field->pack_length_in_rec() >= 4);
+        assert(field->key_type() == HA_KEYTYPE_BINARY);
+        ts = (uint64)rdb_netbuf_to_uint32(field->field_ptr());
+      } else {
+        assert(field->key_type() == HA_KEYTYPE_ULONGLONG);
+        assert(field->pack_length_in_rec() == ROCKSDB_SIZEOF_TTL_RECORD);
+        ts = uint8korr(field->field_ptr());
+      }
+
 #ifndef NDEBUG
       ts += rdb_dbug_set_ttl_rec_ts();
 #endif

storage/rocksdb/rdb_datadic.cc
@@ -547,10 +547,11 @@ void Rdb_key_def::setup(const TABLE *const tbl,
           the offset of the TTL key part here.
         */
         if (!m_ttl_column.empty() &&
-            my_strcasecmp(system_charset_info, field->field_name,
-                          m_ttl_column.c_str()) == 0) {
-          assert(field->real_type() == MYSQL_TYPE_LONGLONG);
-          assert(field->key_type() == HA_KEYTYPE_ULONGLONG);
+            !my_strcasecmp(system_charset_info, field->field_name,
+                           m_ttl_column.c_str())) {
+          assert((field->real_type() == MYSQL_TYPE_LONGLONG &&
+                  field->key_type() == HA_KEYTYPE_ULONGLONG) ||
+                 field->type() == MYSQL_TYPE_TIMESTAMP);
           assert(!field->is_nullable());
           m_ttl_pk_key_part_offset = dst_i;
         }
@@ -681,8 +682,10 @@ uint Rdb_key_def::extract_ttl_col(const TABLE *const table_arg,
       Field *const field = table_arg->field[i];
       if (my_strcasecmp(system_charset_info, field->field_name,
                         ttl_col_str.c_str()) == 0 &&
-          field->real_type() == MYSQL_TYPE_LONGLONG &&
-          field->key_type() == HA_KEYTYPE_ULONGLONG && !field->is_nullable()) {
+          (field->type() == MYSQL_TYPE_TIMESTAMP ||
+           (field->real_type() == MYSQL_TYPE_LONGLONG &&
+            field->key_type() == HA_KEYTYPE_ULONGLONG)) &&
+          !field->is_nullable()) {
         *ttl_column = ttl_col_str;
         *ttl_field_index = i;
         found = true;