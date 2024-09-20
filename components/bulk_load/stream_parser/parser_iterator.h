02b28c4ddd88a08b1171ea0c25a783635789d4be
Oleksandr Kachan <oleksandr.kachan@percona.com>
2024-09-20 16:26:56 +0300

    PS-9187: Bulk ingest feature

---
+++ components/bulk_load/stream_parser/parser_iterator.h
@@ -0,0 +1,106 @@
+/* Copyright (c) 2024, Percona and/or its affiliates.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License, version 2.0,
+   as published by the Free Software Foundation.
+
+   This program is also distributed with certain software (including
+   but not limited to OpenSSL) that is licensed under separate terms,
+   as designated in a particular file or component or in included license
+   documentation. The authors of MySQL hereby grant you an additional
+   permission to link the program and your derivative works with the
+   separately licensed software that they have included with MySQL.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License, version 2.0, for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA */
+
+#pragma once
+
+#include "parser_result.h"
+#include "parser_row.h"
+
+#include <coroutine>
+#include <iterator>
+
+namespace Bulk_load {
+
+struct ParserGenerator {
+
+struct promise_type {
+  ParserResult m_parser_result;
+
+  void unhandled_exception() noexcept {}
+  ParserGenerator get_return_object() { return ParserGenerator{*this}; }
+  std::suspend_always initial_suspend() noexcept { return {}; }
+  std::suspend_always yield_value(ParserResult result) noexcept {
+    m_parser_result = result;
+    return {};
+  }
+  void return_value(ParserResult result) { m_parser_result = result; }
+  std::suspend_always final_suspend() noexcept { return {}; }
+};
+
+class iterator {
+ public:
+//  iterator() = default;
+  explicit iterator(ParserGenerator *generator) : m_generator{generator} {}
+
+  bool operator==(std::default_sentinel_t) const {
+    return m_generator->m_handle.done();
+  }
+
+  iterator &operator++() {
+    if (m_generator !=nullptr && m_generator->m_handle) {
+      m_generator->m_handle.resume();
+    }
+    return *this;
+  }
+
+  iterator& operator++(int) {
+    return ++(*this);
+  }
+
+  const ParserResult *operator*() const {
+    return &m_generator->m_handle.promise().m_parser_result;
+  }
+
+ private:
+  ParserGenerator *m_generator;
+};
+
+std::coroutine_handle<promise_type> m_handle{};
+
+explicit ParserGenerator(promise_type &promise)
+    : m_handle{std::coroutine_handle<promise_type>::from_promise(promise)} {}
+
+ParserGenerator(ParserGenerator &&iter) noexcept
+    : m_handle{std::exchange(iter.m_handle, nullptr)} {}
+ParserGenerator &operator=(ParserGenerator &&iter) noexcept {
+  m_handle = std::exchange(iter.m_handle, nullptr);
+  return *this;
+}
+
+ParserGenerator(const ParserGenerator &iter) = delete;
+ParserGenerator &operator=(const ParserGenerator &iter) = delete;
+
+~ParserGenerator() noexcept {
+  if (m_handle) {
+    m_handle.destroy();
+  }
+}
+
+iterator begin() {
+  auto it = iterator{this};
+  return ++it;
+}
+
+std::default_sentinel_t end() { return {}; }
+};
+
+}  // namespace Bulk_load
---
+++ components/bulk_load/stream_parser/stream_parser.cc
@@ -0,0 +1,152 @@
+/* Copyright (c) 2024, Percona and/or its affiliates.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License, version 2.0,
+   as published by the Free Software Foundation.
+
+   This program is also distributed with certain software (including
+   but not limited to OpenSSL) that is licensed under separate terms,
+   as designated in a particular file or component or in included license
+   documentation. The authors of MySQL hereby grant you an additional
+   permission to link the program and your derivative works with the
+   separately licensed software that they have included with MySQL.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License, version 2.0, for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA */
+
+#include "stream_parser.h"
+
+#include <mysql/components/services/log_builtins.h>
+#include <mysqld_error.h>
+
+#include <cassert>
+#include <coroutine>
+#include <iostream>
+#include <unordered_map>
+#include <functional>
+
+namespace Bulk_load {
+namespace {
+
+}  // namespace
+
+StreamParser::StreamParser(size_t num_columns,
+                           ParserParamsPtr parser_params,
+                           DataStreamAbstract *data_stream)
+  : m_parser_params{std::move(parser_params)},
+    m_data_stream{data_stream},
+    m_unparsed_stream_size{m_data_stream->get_data_size()},
+    m_row{num_columns} {}
+
+bool StreamParser::read_stream() noexcept {
+  if (m_unparsed_stream_size == 0) {
+    return true;
+  }
+
+  const auto len_to_read = std::min<size_t>(m_unparsed_stream_size,
+                                            m_parser_params->m_buffer_size);
+  const auto read_len = m_data_stream->read(m_buffer.get(), len_to_read);
+
+  if (read_len == 0) {
+    LogComponentErr(ERROR_LEVEL, ER_BULK_LOADER_COMPONENT_ERROR,
+                    "Failed to read data");
+    return false;
+  }
+
+  m_unparsed_stream_size -= read_len;
+  m_buffer_ptr = m_buffer_begin;
+  m_buffer_end = m_buffer_begin + read_len;
+
+  return true;
+}
+
+StreamParser::State StreamParser::handle_column_enclose_char() noexcept {
+  if (m_column_begin == nullptr) {
+    m_column_begin = m_buffer_ptr + 1;
+    return State::GetColumnEnd;
+  }
+
+  if (m_column_end == nullptr) {
+    m_column_end = m_buffer_ptr;
+    m_row.m_column_ptrs[m_column_idx] = m_column_begin;
+    m_row.m_column_lens[m_column_idx] = m_column_end - m_column_begin;
+    return (m_column_idx < m_row.m_num_columns)
+               ? State::GetColumnStart : State::GetRowEnd;
+  }
+
+  return State::Error;
+}
+
+StreamParser::State StreamParser::handle_column_terminator() noexcept {
+  ++m_column_idx;
+  m_column_begin = nullptr;
+  m_column_end = nullptr;
+  return State::GetColumnStart;
+}
+
+StreamParser::State StreamParser::handle_row_terminator() noexcept {
+  m_column_idx = 0;
+  m_column_begin = nullptr;
+  m_column_end = nullptr;
+  return State::RowReady;
+}
+
+ParserGenerator StreamParser::row_iterator() noexcept {
+  static std::unordered_map<char, State (StreamParser::*)()> handler = {
+    {static_cast<char>(m_parser_params->m_column_enclose_char),
+       &StreamParser::handle_column_enclose_char},
+    {m_parser_params->m_column_terminator[0],
+       &StreamParser::handle_column_terminator},
+    {m_parser_params->m_row_terminator[0],
+       &StreamParser::handle_row_terminator},
+  };
+
+  m_buffer.reset(static_cast<char *>(malloc(m_parser_params->m_buffer_size)));
+
+  if (m_buffer == nullptr) {
+    LogComponentErr(ERROR_LEVEL, ER_BULK_LOADER_COMPONENT_ERROR,
+                    "Failed to allocate buffer");
+    co_return {true, nullptr};
+  }
+
+  m_buffer_begin = m_buffer.get();
+  m_buffer_end = m_buffer_begin;
+  m_buffer_ptr = m_buffer_begin;
+
+  m_column_begin = nullptr;
+  m_column_end = nullptr;
+
+  auto state = State::Start;
+
+  while (m_unparsed_stream_size > 0) {
+    if (m_buffer_ptr == m_buffer_end && !read_stream()) {
+      co_return {true, nullptr};
+    }
+
+    while (m_buffer_ptr != m_buffer_end) {
+      if (handler.contains(*m_buffer_ptr)) {
+        state = (this->*handler[*m_buffer_ptr])();
+      }
+
+      if (state == State::RowReady) {
+        co_yield {false, &m_row};
+        ++m_row.m_row_idx;
+      }
+      else if (state == State::Error) {
+        co_return {true, nullptr};
+      }
+
+      ++m_buffer_ptr;
+    }
+  }
+
+  co_return {false, nullptr};
+}
+
+}  // namespace Bulk_load
